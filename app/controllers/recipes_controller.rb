dfff622cacb2766a4a2b4efe5abf4706efd785fc
Abdul Khan <akhan17@outlook.com>
2020-07-29 18:03:55 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -16,7 +16,7 @@ class RecipesController < ApplicationController
 
   private
 
-  def recipe_params 
+  def recipe_params
     params.require(:recipe).permit(
       :title,
       ingredients_attributes: [
